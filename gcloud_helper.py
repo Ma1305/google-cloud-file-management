4104dc361f0aaa94c803c04483885f21a82b6987
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-07-23 01:10:08 -0400

    method for comparing files from google cloud using crc32c (without downloading the file for checking)

--- gcloud_helper.py
+++ gcloud_helper.py
@@ -1,4 +1,6 @@
 import os
+import crcmod
+import base64
 
 
 def upload_file(file_name, blob_name, bucket):
@@ -7,6 +9,7 @@ def upload_file(file_name, blob_name, bucket):
         [print(item) for item in dir(blob)]
         blob.upload_from_filename(file_name)
     except Exception as e:
+        print(f"following error occurred when uploading {file_name} as {blob_name} in {bucket.name} bucket")
         print(e)
         return False
 
@@ -16,6 +19,7 @@ def download_file(file_name, blob_name, bucket):
         blob = bucket.blob(blob_name)
         blob.download_to_filename(file_name)
     except Exception as e:
+        print(f"following error occurred when downloading {blob_name} in {bucket.name} bucket as {file_name} ")
         print(e)
         return False
 
@@ -25,6 +29,7 @@ def delete_file(blob_name, bucket):
         blob = bucket.blob(blob_name)
         blob.delete()
     except Exception as e:
+        print(f"following error occurred when deleting {blob_name} in {bucket.name}")
         print(e)
         return False
 
@@ -37,6 +42,13 @@ def download_all(bucket):
 
 
 def create_missing_directory(filepath, prefix=""):
+    """
+    Do not use this method unless you fully understand it.
+    It is meant as a helper method, not to be used in other code.
+    :param filepath:
+    :param prefix:
+    :return:
+    """
     directory_from_index = filepath.find("/")
     if directory_from_index == -1:
         return
@@ -50,3 +62,13 @@ def create_missing_directory(filepath, prefix=""):
         os.mkdir(prefix + directory_name)
 
     create_missing_directory(filepath, prefix=prefix + directory_name + "/")
+
+
+def is_file_different(file_name, blob_name, bucket):
+    blob = bucket.get_bucket(blob_name)
+    with open(file_name) as file:
+        file_bytes = file.read()
+        crc32c = crcmod.predefined.Crc('crc-32c')
+        crc32c.update(file_bytes)
+
+        return blob.crc32c == base64.b64encode(crc32c.digest()).decode('utf-8')
